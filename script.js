ae0bd85e0176b647e4242ab03c8fe8b8f2e0814d
felle64 <felle641@gmail.com>
2023-01-05 21:25:26 +0100

    racket working

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-import Ball from "./gameFiles/ball";
+import Ball from "./gameFiles/ball.js";
+import Racket from "./gameFiles/racket.js";
 
-const ball = new Ball(document.getElementById("ball"))
+//const ball = new Ball(document.getElementById("ball"))
 
-const delta = time - lastTime
+//const delta = time - lastTime
+
+
+const playerRacket = new Racket(document.getElementById("playerPadell"))
+const computerRacket = new Racket(document.getElementById("computerPadell"))
+
+document.addEventListener(("mousemove"), e => {
+    playerRacket.position = e.y / window.innerHeight * 100
+})
